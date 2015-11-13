acf0ddb824b4d8269b59f4aa7e030af734a8eb59
gpedroemail@gmail.com <gpedroemail@gmail.com>
2015-11-12 23:53:30 -0200

    Atualizamos a URL de base de acordo com o nosso projeto e removemos o index.php da configuração index_page

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://127.0.0.1/CursoCodeIgniter/';
+$config['base_url'] = 'http://127.0.0.1/DevIgniter/Sistema-Im-DevIgniter/im-devigniter/';
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ $config['base_url'] = 'http://127.0.0.1/CursoCodeIgniter/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
